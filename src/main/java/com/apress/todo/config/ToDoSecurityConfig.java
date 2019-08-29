66bfa7bae43a1c7676a32a08f11b3d1d5f80e397
axie <axie@swin.edu.au>
2019-08-29 13:50:58 +1000

    change to call db authendication

--- src/main/java/com/apress/todo/config/ToDoSecurityConfig.java
+++ src/main/java/com/apress/todo/config/ToDoSecurityConfig.java
@@ -1,17 +1,36 @@
 package com.apress.todo.config;
 
+import com.apress.todo.directory.Person;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.core.annotation.Order;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.hateoas.Resource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
 
 @EnableWebSecurity
 //https://docs.spring.io/spring-security/site/docs/4.2.x/reference/htmlsingle/#multiple-httpsecurity
@@ -19,7 +38,19 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
  * WebSecurityConfigurerAdapter. Extending this class is one way to override security because it allows you to override
  * the methods that you really need. In this case, the code overrides the configure(AuthenticationManagerBuilder) signature.
  **/
+@EnableConfigurationProperties(ToDoProperties.class)
+@Configuration
 public class ToDoSecurityConfig {
+
+  private final Logger log = LoggerFactory.getLogger(ToDoSecurityConfig.class);
+  //Use this to connect to the Directory App
+  private RestTemplate restTemplate;
+  private ToDoProperties toDoProperties;
+  private UriComponentsBuilder builder;
+  public ToDoSecurityConfig(RestTemplateBuilder restTemplateBuilder, ToDoProperties toDoProperties){
+    this.toDoProperties = toDoProperties;
+    this.restTemplate = restTemplateBuilder.basicAuthentication(toDoProperties.getUsername(), toDoProperties.getPassword()).build();
+  }
  // @Override
   /**
    * AuthenticationManagerBuilder. This class creates an AuthenticationManager that allows you to easily build in memory,
@@ -38,10 +69,43 @@ public class ToDoSecurityConfig {
 
   @Bean
   public UserDetailsService userDetailsService() throws Exception {
-    InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+    /*InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
     manager.createUser(User.withUsername("apress").password(passwordEncoder().encode("springboot2")).roles("USER","ADMIN").build());
     manager.createUser(User.withUsername("admin").password(passwordEncoder().encode("password")).roles("USER","ADMIN").build());
-    return manager;
+    return manager;*/
+    return new UserDetailsService() {
+      @Override
+      public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        try {
+          builder = UriComponentsBuilder
+              .fromUriString(toDoProperties.getFindByEmailUri())
+              .queryParam("email", username);
+          log.info("Querying: " + builder.toUriString());
+          ResponseEntity<Resource<Person>> responseEntity =
+              restTemplate.exchange(
+                  RequestEntity.get(URI.create(builder.toUriString()))
+                      .accept(MediaTypes.HAL_JSON)
+                      .build()
+                  , new ParameterizedTypeReference<Resource<Person>>() {
+                  });
+          if (responseEntity.getStatusCode() == HttpStatus.OK) {
+            Resource<Person> resource = responseEntity.getBody();
+            Person person = resource.getContent();
+            PasswordEncoder encoder = passwordEncoder();
+                //PasswordEncoderFactories.createDelegatingPasswordEncoder();
+            String password = encoder.encode(person.getPassword());
+            return User
+                .withUsername(person.getEmail())
+                .password(password)
+                .accountLocked(!person.isEnabled())
+                .roles(person.getRole()).build();
+          }
+        } catch (Exception ex) {
+          ex.printStackTrace();
+        }
+        throw new UsernameNotFoundException(username);
+      }
+    };
   }
 
   @Bean
@@ -85,7 +149,7 @@ public class ToDoSecurityConfig {
       http
           .antMatcher("/api/**")
           .authorizeRequests()
-          .anyRequest().hasRole("ADMIN")
+          .anyRequest().hasRole("USER")
           .and()
           .httpBasic();
     }
@@ -103,6 +167,7 @@ public class ToDoSecurityConfig {
                   .toStaticResources()
                   .atCommonLocations()).permitAll()
           .anyRequest().fullyAuthenticated()//uses anyRequest, which should be fullyAuthenticated this means that the /api/*
+          .antMatchers("/","/api/**").hasRole("USER")
           .and()
           .formLogin().loginPage("/login").permitAll()
           .and()
